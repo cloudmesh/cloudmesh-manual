694490714970f477f5dc9d603b4a4a2c9eb2d37b
Gregor von Laszewski <laszewski@gmail.com>
2020-03-27 08:49:13 -0400

    add book and update manual

---
+++ cloudmesh/book/command/book.py
@@ -0,0 +1,95 @@
+from __future__ import print_function
+from cloudmesh.shell.command import command
+from cloudmesh.shell.command import PluginCommand
+from cloudmesh.common.console import Console
+from cloudmesh.common.util import path_expand
+from cloudmesh.common.util import banner
+from pprint import pprint
+from cloudmesh.common.debug import VERBOSE
+from glob import glob
+import os
+from cloudmesh.common.Printer import Printer
+
+class BookCommand(PluginCommand):
+
+    # noinspection PyUnusedLocal
+    @command
+    def do_book(self, args, arguments):
+        """
+        ::
+
+          Usage:
+            book proceedings
+            book lecture
+            book cloud
+            book pi
+            book list [--dir=DIR]
+            book make BOOK TARGET
+            book view BOOK [-o]
+
+          Command to view the books. This command must be executed in
+          the cm directory.
+
+          Arguments:
+            BOOK   The name of the book
+
+          Options:
+            -o         open the location to find the online source
+            --dir=DIR  The directory where the different bok dirs are
+                       located.
+                       [default: book/books]
+
+
+        """
+
+        VERBOSE(arguments)
+
+        directory = arguments["--dir"]
+        book = arguments["BOOK"]
+        target = arguments["TARGET"]
+
+        def list():
+            found = {}
+            books = glob(directory + "/**/*.yaml")
+            for book in books:
+                name = os.path.basename((book)).replace(".yaml", "")
+                dirname = os.path.dirname((book)).split("/")[-1]
+                found[name] = {
+                    'name': name,
+                    'dir': dirname,
+                    'path': book
+                }
+            return found
+
+        def make(book, target=""):
+            books = list()
+            _directory = os.path.dirname(books[book]["path"])
+            if not os.path.isfile(f"{_directory}/Makeile"):
+                Console.error("Makefile not found")
+                return ""
+            command = f"cd {_directory}; make {target}"
+            banner(command)
+            os.system(command)
+
+        if arguments.list:
+
+            books = glob(directory + "/**/*.yaml")
+            books = list()
+            print(Printer.write(books, order=['name', 'dir']))
+
+        elif arguments.make:
+            make (book, target)
+
+        elif arguments.pi:
+            make("pi")
+
+        elif arguments.cloud:
+            make("cloud")
+
+        elif arguments.proceedings:
+            make("e516-sp20-proceedings", "proceedings")
+
+        elif arguments.lecture:
+            make("e516-sp20-proceedings")
+
+        return ""
--- docs-source/source/preface/authors.md
+++ docs-source/source/preface/authors.md
@@ -1,15 +1,16 @@
 
-    *Alex Axthelm*, *Andrew Holland*, *Anthon van der Neut*, *Anthony Duer*,
-    *Aravind Mannarswamy*, *Arshad Moeen*, *Ashok*, *Badi Abdul-Wahid*, *Bo Feng*,
-    *Brian Funk*, *Chenxu*, *Dave DeMeulenaere*, *Eric Collins*, *Fugang Wang*,
-    *Gerald Manipon*, *Gregor von Laszewski*, *Harsha Upadhyay*, *Himanshu Gupta*,
-    *Jagadeesh Kandimalla*, *Jeevan Reddy Rachepalli*, *Jing Huang*, *Joaquin Avila
-    Eggleton*, *Jonathan Beckford*, *Karthick*, *Keli Fine*, *Ketan Pimparkar*,
-    *KetanP*, *Kimball Wu*, *Mallik Challa*, *Manjunath Sivan*, *Nayeemullah Baig
-    Shaik*, *Niranda Perera*, *Ritesh Tandon*, *Rob Ludwig*, *Rui Li*, *Sachith
-    Danushka Withana*, *Saurabh Swaroop*, *Scott McClary*, *Shivani Katukota*,
-    *Shreyans Jain*, *Sriman*, *Subramaniyam Raizada*, *Swaroop, Saurabh*, *Tarun
-    Rawat*, *Tharak Vangalapat*, *Vafa Andalibi*, *Xiao Yue*, *Yu Luo*, *Yue,
-    Xiao*, *Zhengyi Li*, *andrewgoldfarb*, *baker203*, *bkegerreis*, *gvonlasz*,
-    *iumsds*, *prateekazam@gmail.com*
+    *Alex Axthelm*, *Andrew Goldfarb*, *Andrew Holland*, *Anthon van der Neut*,
+    *Anthony Duer*, *Aravind Mannarswamy*, *Arshad Moeen*, *Ashok*, *Badi Abdul-
+    Wahid*, *Bo Feng*, *Brian Funk*, *Chenxu*, *Dave DeMeulenaere*, *Eric Collins*,
+    *FalconiNicasio*, *Fugang Wang*, *Gerald Manipon*, *Gregor von Laszewski*,
+    *Harsha Upadhyay*, *Himanshu Gupta*, *Jagadeesh Kandimalla*, *Jeevan Reddy
+    Rachepalli*, *Jing Huang*, *Joaquin Avila Eggleton*, *Jonathan Beckford*,
+    *Karthick*, *Keli Fine*, *Ketan Pimparkar*, *KetanP*, *Kimball Wu*, *Lamcloud*,
+    *Mallik Challa*, *Manjunath Sivan*, *Nayeemullah Baig Shaik*, *Niranda Perera*,
+    *Ritesh Tandon*, *Rob Ludwig*, *Rui Li*, *Sachith Danushka Withana*, *Saurabh
+    Swaroop*, *Scott McClary*, *Shivani Katukota*, *Shreyans Jain*, *Sriman*,
+    *Subramaniyam Raizada*, *Swaroop, Saurabh*, *Tarun Rawat*, *Tharak Vangalapat*,
+    *Vafa Andalibi*, *Xiao Yue*, *Yu Luo*, *Yue, Xiao*, *Zhengyi Li*,
+    *andrewgoldfarb*, *baker203*, *bkegerreis*, *gvonlasz*, *imishra1*, *iumsds*,
+    *prateekazam@gmail.com*, *rahul*
 
